f8c6c2736f0fbc679dd45c1865897364f9e117cc
Zhuohua Huang <zhuohua.huang1@gmail.com>
2024-04-23 17:53:48 +0800

    second commit

---
+++ lang_assist.py
@@ -0,0 +1,48 @@
+
+from langchain_community.document_loaders import JSONLoader
+from langchain_community.vectorstores import FAISS
+import openai_model
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+
+
+LLM_model = openai_model.ChatLLM()
+llm = LLM_model.get_llm()
+
+### Construct retriever ###
+loader = JSONLoader(
+    file_path='./data/all_courses_v2.json',
+    jq_schema='.[].text',
+    text_content=False
+)
+docs = loader.load()
+
+embeddings = LLM_model.get_embeddings()
+
+vectorstore = FAISS.from_documents(docs, embeddings)
+vectorstore.save_local(folder_path='vectorstore', index_name="course_info")
+
+vectorstore.load_local(folder_path='vectorstore', index_name="course_info", embeddings=embeddings)
+retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 5})
+
+template = """Use the following pieces of context to answer the question at the end.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+Use three sentences maximum and keep the answer as concise as possible.
+Always say "thanks for asking!" at the end of the answer.
+
+{context}
+
+Question: {question}
+
+Helpful Answer:"""
+custom_rag_prompt = PromptTemplate.from_template(template)
+
+rag_chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | custom_rag_prompt
+    | llm
+    | StrOutputParser()
+)
+for chunk in rag_chain.stream("What is Task Decomposition?"):
+    print(chunk, end="", flush=True)
