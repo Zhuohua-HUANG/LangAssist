e91add24fc8f14360e7516701194bbc29d952b6e
llag9810 <llag9810@outlook.com>
2024-04-24 14:24:31 +0800

    finish test

--- lang_assist.py
+++ lang_assist.py
@@ -11,20 +11,19 @@ LLM_model = openai_model.ChatLLM()
 llm = LLM_model.get_llm()
 
 ### Construct retriever ###
-loader = JSONLoader(
-    file_path='./data/all_courses_v2.json',
-    jq_schema='.[].text',
-    text_content=False
-)
-docs = loader.load()
-
+# loader = JSONLoader(
+#     file_path='./data/all_courses_v2.json',
+#     jq_schema='.[].text',
+#     text_content=False
+# )
+# docs = loader.load()
+#
 embeddings = LLM_model.get_embeddings()
-
-vectorstore = FAISS.from_documents(docs, embeddings)
-vectorstore.save_local(folder_path='vectorstore', index_name="course_info")
-
-vectorstore.load_local(folder_path='vectorstore', index_name="course_info", embeddings=embeddings)
-retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 5})
+#
+# vectorstore = FAISS.from_documents(docs, embeddings)
+# vectorstore.save_local(folder_path='vectorstore', index_name="course_info")
+vectorstore = FAISS.load_local(folder_path='vectorstore', index_name="course_info", embeddings=embeddings, allow_dangerous_deserialization=True)
+retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 10})
 
 template = """Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
@@ -44,5 +43,5 @@ rag_chain = (
     | llm
     | StrOutputParser()
 )
-for chunk in rag_chain.stream("What is Task Decomposition?"):
+for chunk in rag_chain.stream("I am a prospective student and I want to learn a programme related to computer science. Can you compare the courses and differences between information technology and big data technology?"):
     print(chunk, end="", flush=True)
